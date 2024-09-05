43299d9f72f0dfa0229563c63f2862af90073d85
DEIDADEV <161657805+DEIDADEV@users.noreply.github.com>
2024-09-04 23:23:45 -0300

    commitando alterações = 04.09

---
+++ src/main/java/br/com/cezardev/repository/PermissaoPerfilRecursoRepository.java
@@ -0,0 +1,9 @@
+package br.com.cezardev.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.cezardev.entity.PermissaoPerfilRecursoEntity;
+
+public interface PermissaoPerfilRecursoRepository extends JpaRepository<PermissaoPerfilRecursoEntity, Long>{
+
+}
